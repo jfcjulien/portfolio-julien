9aa01dfb4bc15c1ad73858ee15983340a9cf2262
Julien <julconig297@gmail.com>
2025-10-18 12:36:07 +0000

    Refonte

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
 $host = "localhost";
 $dbname = "portfolio_db";
-$username = "julien";
-$password = "8vcu35jhtk7evz45urxr";
+$username = "portfolio_user";
+$password = "";
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // echo "Connexion réussie !"; // tu peux décommenter pour tester
 } catch (PDOException $e) {
   die("Erreur de connexion : " . $e->getMessage());
 }
